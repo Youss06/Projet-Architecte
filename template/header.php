1ac19e59bec6ac27bf68f7c6102c3a201eec8dc9
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-28 09:31:19 +0200

    Add Boilerplate

---
+++ template/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/main.css">
+    <title>Architecte</title>
+  </head>
+  <body>
+<header>
+  <!-- <div class="menu">
+  <div class="menu--text desktop">
+  <div class="row"> -->
+    <h1>Architecte</h1>
+    <h2>réinventons le design</h2>
+  <!-- </div>
+  </div>
+</div> -->
+</header>
+  </body>
+</html>
