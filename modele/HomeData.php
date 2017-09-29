f967bea61cf7598fb2390b9f5c998fcc66de4a11
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-29 14:58:55 +0200

    Add modele home data

--- modele/HomeData.php
+++ modele/HomeData.php
@@ -15,7 +15,6 @@ function projet($nomProjet, $infos, $date ){
 global $bdd;
   $req = $bdd->prepare('INSERT INTO Projet(nom_Projet, infos, date_limite)
   VALUES(:nom_Projet, :infos, :date_limite )');
-
   $req->execute(array(
     'nom_Projet'=>$nomProjet,
     'infos'=>$infos,
